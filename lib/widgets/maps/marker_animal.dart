284f2f82c9532ff91cffc15e3c997445985ffc40
Jan Stehno <honzastehno@email.cz>
2024-06-07 21:09:23 +0200

    Added longTap option for animal marker

--- lib/widgets/maps/marker_animal.dart
+++ lib/widgets/maps/marker_animal.dart
@@ -7,23 +7,30 @@ import 'package:wothcompanion/model/map/animal_location.dart';
 import 'package:wothcompanion/widgets/maps/marker.dart';
 
 class WidgetMarkerAnimal extends WidgetMarker<AnimalLocation> {
+  final Color _color;
   final Function _onTap;
+  final Function _onLongTap;
 
   const WidgetMarkerAnimal(
     super.location, {
     super.key,
     required super.offset,
     required double size,
+    required Color color,
     required Function onTap,
-  })  : _onTap = onTap,
+    required Function onLongTap,
+  })  : _color = color,
+        _onTap = onTap,
+        _onLongTap = onLongTap,
         super(width: size, height: size);
+
+  Color get _actualColor => location.isShownDetail ? Interface.markerAnimal : _color;
 
   Widget _buildFill() {
     return SvgPicture.asset(
       Graphics.getAnimalLocation(location.animal, MarkerType.fill),
       fit: BoxFit.fitWidth,
-      colorFilter:
-          ColorFilter.mode(location.isShownDetail ? Interface.markerAnimal : Interface.primaryLight, BlendMode.srcIn),
+      colorFilter: ColorFilter.mode(_actualColor, BlendMode.srcIn),
     );
   }
 
@@ -39,6 +46,7 @@ class WidgetMarkerAnimal extends WidgetMarker<AnimalLocation> {
   Widget buildCenter() {
     return GestureDetector(
       onTap: () => _onTap(),
+      onLongPress: () => _onLongTap(),
       child: Stack(
         children: [
           _buildFill(),
